42d52abaa23697349615a229420f590d95bb6e73
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2025-06-04 00:54:45 +0530

    Add assertions for semantic tokens in did_save and did_change tests

--- jac/jaclang/langserve/tests/server_test/test_lang_serve.py
+++ jac/jaclang/langserve/tests/server_test/test_lang_serve.py
@@ -181,6 +181,9 @@ class TestLangServe:
             )
         )
         await did_save(ls, params)
+        sem_tokens = ls.get_semantic_tokens(uri)
+        # semantic tokens should still be present even if there is a syntax error
+        assert len(sem_tokens.data) == 300
         diagnostics = ls.diagnostics.get(uri, [])
         assert isinstance(diagnostics, list)
         assert len(diagnostics) == 1
@@ -235,6 +238,9 @@ class TestLangServe:
             text=error_code + code,
         ))
         await did_change(ls, params)
+        sem_tokens = ls.get_semantic_tokens(uri)
+        # semantic tokens should still be present even if there is a syntax error
+        assert len(sem_tokens.data) == 300
         diagnostics = ls.diagnostics.get(uri, [])
         assert isinstance(diagnostics, list)
         assert len(diagnostics) == 1
