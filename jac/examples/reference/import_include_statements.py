77b029c286acfabd17b0d29758fa7b7caa0b2642
marsninja <jason@mars.ninja>
2025-06-10 22:03:15 -0400

    codex didnt do it but i did hehe

--- jac/examples/reference/import_include_statements.py
+++ jac/examples/reference/import_include_statements.py
@@ -1,8 +1,10 @@
 import os
 import datetime as dt
 from math import sqrt as square_root, log
+import sys
 
-from .base_module_structure import add, subtract
+sys.path.append(os.path.dirname(__file__))
+from base_module_structure import add, subtract
 
 for i in range(int(square_root(dt.datetime.now().year))):
     print(os.getcwd(), add(i, subtract(i, 1)), int(log(i + 1)))
