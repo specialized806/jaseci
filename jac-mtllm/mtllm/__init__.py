jaseci-labs/jac

🤖 LiteLLM Integration for MTLLM

jac-mtllm/mtllm/__init__.py
@@ -1,5 +1,6 @@
 """MTLLM Package."""
 
+from mtllm.llm import JacLLM
 from mtllm.types import Image, Video
 
-__all__ = ["Image", "Video"]
+__all__ = ["Image", "JacLLM", "Video"]

specialized806/jaseci

jac-mtllm/mtllm/__init__.py
@@ -1,6 +1,6 @@
 """MTLLM Package."""
 
 from mtllm.llm import Model
-from mtllm.types import Image, MockToolCall, Video
+from mtllm.types import Image, Video
 
-__all__ = ["Image", "MockToolCall", "Model", "Video"]
+__all__ = ["Image", "Model", "Video"]