7428570dc551c0d66f7ddf3bd9cf630d38fb0fc1
Alexie (Boyong) Madolid <maex@tuta.io>
2025-09-16 21:16:30 +0800

    [REVERTIBLE]: Remove jac_import overrides as we don't need it yet

--- jac-splice-orc/jac_splice_orc/proxy/plugin/proxy.py
+++ jac-splice-orc/jac_splice_orc/proxy/plugin/proxy.py
@@ -1,11 +1,6 @@
 """JAC Splice-Orchestrator Plugin."""
 
-from types import ModuleType
-from typing import Optional, Union
-
 from jaclang.cli.cmdreg import cmd_registry
-from jaclang.runtimelib.importer import ImportPathSpec, JacImporter, PythonImporter
-from jaclang.runtimelib.machine import JacMachine, JacProgram
 
 from pluggy import HookimplMarker
 
@@ -24,47 +19,3 @@ class ProxyPlugin:
         def verify() -> bool:
             """Verify Installation."""
             return True
-
-    @staticmethod
-    @hookimpl
-    def jac_import(
-        target: str,
-        base_path: str,
-        absorb: bool,
-        cachable: bool,
-        mdl_alias: Optional[str],
-        override_name: Optional[str],
-        lng: Optional[str],
-        items: Optional[dict[str, Union[str, Optional[str]]]],
-        reload_module: Optional[bool],
-    ) -> tuple[ModuleType, ...]:
-        """Core Import Process with Kubernetes Pod Integration."""
-        spec = ImportPathSpec(
-            target,
-            base_path,
-            absorb,
-            cachable,
-            mdl_alias,
-            override_name,
-            lng,
-            items,
-        )
-
-        jac_machine = JacMachine.get(base_path)
-        if not jac_machine.jac_program:
-            jac_machine.attach_program(
-                JacProgram(mod_bundle=None, bytecode=None, sem_ir=None)
-            )
-
-        if lng == "py":
-            import_result = PythonImporter(JacMachine.get()).run_import(spec)
-        else:
-            import_result = JacImporter(JacMachine.get()).run_import(
-                spec, reload_module
-            )
-
-        return (
-            (import_result.ret_mod,)
-            if absorb or not items
-            else tuple(import_result.ret_items)
-        )
