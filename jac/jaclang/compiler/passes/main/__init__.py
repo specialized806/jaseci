f126a0ce5f3eedda28f2d8a512510a135c2fe68c
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2025-09-19 11:31:33 +0530

    remove unused inheritance-related code and clean up symbol table handling

--- jac/jaclang/compiler/passes/main/__init__.py
+++ jac/jaclang/compiler/passes/main/__init__.py
@@ -4,7 +4,6 @@ from ..transform import Alert, Transform  # noqa: I100
 from .annex_pass import JacAnnexPass  # noqa: I100
 from .binder_pass import BinderPass  # noqa: I100
 from .sym_tab_build_pass import SymTabBuildPass, UniPass  # noqa: I100
-# from .sym_tab_link_pass import SymTabLinkPass  # noqa: I100
 from .def_use_pass import DefUsePass  # noqa: I100
 from .sem_def_match_pass import SemDefMatchPass  # noqa: I100
 from .import_pass import JacImportDepsPass  # noqa: I100
@@ -15,7 +14,6 @@ from .pyast_gen_pass import PyastGenPass  # noqa: I100
 from .pybc_gen_pass import PyBytecodeGenPass  # noqa: I100
 from .cfg_build_pass import CFGBuildPass  # noqa: I100
 from .pyjac_ast_link_pass import PyJacAstLinkPass  # noqa: I100
-# from .inheritance_pass import InheritancePass  # noqa: I100
 
 
 __all__ = [
@@ -28,7 +26,6 @@ __all__ = [
     "BinderPass",
     "TypeCheckPass",
     "SymTabBuildPass",
-    # "SymTabLinkPass",
     "DeclImplMatchPass",
     "DefUsePass",
     "SemDefMatchPass",
@@ -37,5 +34,4 @@ __all__ = [
     "PyBytecodeGenPass",
     "CFGBuildPass",
     "PyJacAstLinkPass",
-    # "InheritancePass",
 ]
