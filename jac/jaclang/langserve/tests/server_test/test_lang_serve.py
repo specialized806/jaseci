c5f1014a45ab455018883fc902b1d9e456f41531
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2025-09-19 11:36:11 +0530

    Remove unused semantic token retrieval and debug print statement in test

--- jac/jaclang/langserve/tests/server_test/test_lang_serve.py
+++ jac/jaclang/langserve/tests/server_test/test_lang_serve.py
@@ -162,8 +162,6 @@ class TestLangServe:
         )
 
         await did_save(ls, params)
-        sem_tokens = ls.get_semantic_tokens(uri)
-        print('ilasessaa>> ',len(sem_tokens.data))
         diagnostics = ls.diagnostics.get(uri, [])
         assert isinstance(diagnostics, list)
         assert len(diagnostics) == 0
