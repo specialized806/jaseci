f6e4378073a047ce5ff7f22b803b815c3f26a162
Thamirawaran <107134124+Thamirawaran@users.noreply.github.com>
2025-09-26 16:19:16 +0530

    Test cases added

--- jac/jaclang/compiler/passes/main/tests/test_predynamo_pass.py
+++ jac/jaclang/compiler/passes/main/tests/test_predynamo_pass.py
@@ -41,3 +41,15 @@ class PreDynamoPassTests(TestCase):
         self.assertIn("torch.where", code_gen.unparse())
         os.environ["JAC_PREDYNAMO_PASS"] = "false"
         settings.load_env_vars()
+
+    def test_predynamo_fix3(self) -> None:
+        """Test torch.where transformation."""
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        os.environ["JAC_PREDYNAMO_PASS"] = "True"
+        settings.load_env_vars()
+        code_gen = JacProgram().compile(self.fixture_abs_path("predynamo_fix3.jac"))
+        sys.stdout = sys.__stdout__
+        self.assertIn("torch.where", code_gen.unparse())
+        os.environ["JAC_PREDYNAMO_PASS"] = "false"
+        settings.load_env_vars()
