e2be11185226d5c05af21a5537548faf1c373899
Thamirawaran <107134124+Thamirawaran@users.noreply.github.com>
2025-10-01 23:17:31 +0530

    check statements reference merged with assert statements

--- jac/examples/reference/assert_statements.py
+++ jac/examples/reference/assert_statements.py
@@ -1,8 +1,29 @@
-def foo(value: int):
-    assert value > 0, "Value must be positive"
+from __future__ import annotations
+from jaclang.runtimelib.builtin import *
+from jaclang import JacMachineInterface as _jl
 
-
+def foo(value: int) -> None:
+    assert value > 0, 'Value must be positive'
 try:
     foo(-5)
 except AssertionError as e:
-    print("Asserted:", e)
+    print('Asserted:', e)
+    
+a = 5
+b = 2
+
+@_jl.jac_test
+def test_test1(_check) -> None:
+    _check.assertAlmostEqual(a, 6)
+
+@_jl.jac_test
+def test_test2(_check) -> None:
+    _check.assertNotEqual(a, b)
+
+@_jl.jac_test
+def test_test3(_check) -> None:
+    _check.assertIn('d', 'abc')
+
+@_jl.jac_test
+def test_test4(_check) -> None:
+    _check.assertEqual(a - b, 3)
