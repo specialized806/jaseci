c16e97de33ac13875b9b83b2c87a4d1980275758
marsninja <jason@mars.ninja>
2025-10-04 10:26:33 -0400

    test tweak

--- jac/jaclang/compiler/passes/main/tests/test_predynamo_pass.py
+++ jac/jaclang/compiler/passes/main/tests/test_predynamo_pass.py
@@ -22,36 +22,30 @@ class PreDynamoPassTests(TestCase):
         """Test torch.where transformation."""
         captured_output = io.StringIO()
         sys.stdout = captured_output
-        os.environ["JAC_PREDYNAMO_PASS"] = "True"
-        settings.load_env_vars()
+        settings.predynamo_pass = True
         code_gen = JacProgram().compile(self.fixture_abs_path("predynamo_where_assign.jac"))
         sys.stdout = sys.__stdout__
         self.assertIn("torch.where", code_gen.unparse())
-        os.environ["JAC_PREDYNAMO_PASS"] = "false"
-        settings.load_env_vars()
+        settings.predynamo_pass = False
         
     def test_predynamo_where_return(self) -> None:
         """Test torch.where transformation."""
         captured_output = io.StringIO()
         sys.stdout = captured_output
-        os.environ["JAC_PREDYNAMO_PASS"] = "True"
-        settings.load_env_vars()
+        settings.predynamo_pass = True
         code_gen = JacProgram().compile(self.fixture_abs_path("predynamo_where_return.jac"))
         sys.stdout = sys.__stdout__
         self.assertIn("torch.where", code_gen.unparse())
-        os.environ["JAC_PREDYNAMO_PASS"] = "false"
-        settings.load_env_vars()
+        settings.predynamo_pass = False
 
     def test_predynamo_fix3(self) -> None:
         """Test torch.where transformation."""
         captured_output = io.StringIO()
         sys.stdout = captured_output
-        os.environ["JAC_PREDYNAMO_PASS"] = "True"
-        settings.load_env_vars()
+        settings.predynamo_pass = True
         code_gen = JacProgram().compile(self.fixture_abs_path("predynamo_fix3.jac"))
         sys.stdout = sys.__stdout__
         unparsed_code = code_gen.unparse()
         self.assertIn("__inv_freq = torch.where(", unparsed_code)
         self.assertIn("self.register_buffer('inv_freq', __inv_freq, persistent=False);", unparsed_code)
-        os.environ["JAC_PREDYNAMO_PASS"] = "false"
-        settings.load_env_vars()
+        settings.predynamo_pass = False
