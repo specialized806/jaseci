a4f507af8e9cb4f3870dbc8739c435cb0121d0b1
marsninja <jason@mars.ninja>
2025-10-09 13:28:03 -0400

    general tools to fresh_env script

--- scripts/fresh_env.sh
+++ scripts/fresh_env.sh
@@ -2,3 +2,6 @@ pip install -e jac
 pip install -e jac-byllm
 pip install -e jac-cloud
 pip install -e jac-streamlit
+pip install pre-commit
+pre-commit install
+pip install pytest pytest-xdist pytest-asyncio
