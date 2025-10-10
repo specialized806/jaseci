67f801cc0db24fa98b698191fb9a10eaadf13533
Thamirawaran <107134124+Thamirawaran@users.noreply.github.com>
2025-10-11 02:25:57 +0530

    better errors

--- jac/jaclang/compiler/parser.py
+++ jac/jaclang/compiler/parser.py
@@ -155,7 +155,7 @@ class JacParser(Transform[uni.Source, uni.Module]):
         # Reference: https://github.com/lark-parser/lark/blob/master/examples/advanced/error_reporting_lalr.py
         # e.match_examples()
         if isinstance(e, jl.UnexpectedToken):
-            return f"Unexpected token '{e.token.value}'"
+            return f"Unexpected token '{e.token.value}'\n Expected one of: {list(e.accepts)}\n"
         return "Syntax Error"
 
     def error_to_token(self, e: jl.UnexpectedInput) -> uni.Token:
