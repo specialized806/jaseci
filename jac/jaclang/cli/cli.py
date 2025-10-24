66f9c0e395b34e9eec4383e5e2a3a6c078cd40ef
Musab Mahmoodh <180377J@uom.lk>
2025-10-24 11:13:52 +0530

    fix polyfill issue

--- jac/jaclang/cli/cli.py
+++ jac/jaclang/cli/cli.py
@@ -776,6 +776,10 @@ def create_jac_app(name: str) -> None:
             },
             "devDependencies": {
                 "vite": "^5.0.0"
+            },
+            "dependencies": {
+                "react": "^18.2.0",
+                "react-dom": "^18.2.0"
             }
         })
         
