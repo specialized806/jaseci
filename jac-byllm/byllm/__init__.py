ce5d5b4a33c2f35039ee0c79c904c5fcaa1cab67
Jayanaka-98 <jayanaka@umich.edu>
2025-10-31 00:01:33 -0400

    jaclang import error reporting

--- jac-byllm/byllm/__init__.py
+++ jac-byllm/byllm/__init__.py
@@ -1,1 +1,10 @@
 """byLLM Package - Lazy Loading."""
+import importlib.util
+import sys
+
+if importlib.util.find_spec("jaclang") is None:
+    sys.stderr.write(
+        "ImportError: jaclang is required for byLLM to function. "
+        "Please install it via 'pip install jaclang', or reinstall byLLM.\n"
+    )
+    sys.exit(1)
