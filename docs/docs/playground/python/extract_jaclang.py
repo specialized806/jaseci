3a6cce8ba04f87988425711c65a1c81246d835ed
Malitha Prabhashana <110366097+MalithaPrabhashana@users.noreply.github.com>
2025-11-26 20:24:41 +0530

    Remove build files from playground and replaced with workflow (#3494)
    ## **Description**

--- docs/docs/playground/python/extract_jaclang.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-import sys
-
-with zipfile.ZipFile("/jaclang.zip", "r") as zip_ref:
-    zip_ref.extractall("/jaclang")
-
-sys.path.append("/jaclang")
-print("JacLang files loaded!")
